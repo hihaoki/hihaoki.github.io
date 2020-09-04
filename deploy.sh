d1a93af172e6785cf5fdb06eb0109d8a230692e8
hao.huang <1570356753@qq.com>
2020-09-04 17:04:12 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:hihaoki/hihaoi.github.io.git master
+git push -f git@github.com:hihaoki/hihaoki.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:hihaoki/<REPO>.git master:gh-pages
